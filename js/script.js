cca05e9b226e80221153b890c48aa4dc1971ca47
Caio Oliveira <106616005+CaiOliveira19@users.noreply.github.com>
2022-06-20 10:58:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const btnMenu = document.getElementById('btn_menu');
 
 function toggleMenu(){
-    const nav = document.getElementById('#Menu');
+    const nav = document.getElementById('Menu');
+    const navs = document.getElementById('btn_menu');
     nav.classList.toggle('active');
+    navs.classList.toggle('active');
 }
 
 btnMenu.addEventListener('click', toggleMenu);
